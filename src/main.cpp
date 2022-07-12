7e1376d5737b2e4e9dddd734f3370f5ff6957e2f
Cedric Harper <cedricharper@icloud.com>
2022-07-12 11:47:39 -0400

    added 3d vec, refactored 2d vec

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,9 @@ int main()
     Vec2<int> v2{3, 4};
     auto v3 = -v2;
     auto m = v2.magnitude();
-    fmt::print("{}\n", m);
-
+    constexpr auto prod = cross(vec3i{2, 3, 4}, vec3i{5, 6, 7});
+    fmt::print("{},{},{}\n", prod.x, prod.y, prod.z);
+    constexpr auto dprod = dot(vec3i{9, 2, 7}, vec3i{4, 8, 10});
+    fmt::print("{}\n", dprod);
     return 0;
 }
--- src/vec.hpp
+++ src/vec.hpp
@@ -3,50 +3,111 @@
 #include <cmath>
 #include <concepts>
 
+namespace harp{
+
 template<typename T>
-requires std::integral<T> || std::floating_point<T>
+concept numeric = std::integral<T> || std::floating_point<T>;
+
+template<typename T>
+requires numeric<T>
 struct Vec2 
 {
-    T x1 = 0, x2 = 0;
+    T x = 0, y = 0;
     constexpr Vec2<T>() = default;
-    constexpr Vec2<T>(T x1_, T x2_) : x1{x1_}, x2{x2_} {}
+    constexpr Vec2<T>(T x_, T y_) : x{x_}, y{y_} {}
 
     constexpr auto operator+=(Vec2<T> rhs) const
     {
-        return Vec2<T>(x1+rhs.x1, x2+rhs.x2);
+        return Vec2<T>{x+rhs.x, y+rhs.y};
     }
 
     constexpr auto operator-=(Vec2<T> rhs) const
     {
-        return Vec2<T>(x1-rhs.x1, x2-rhs.x2);
+        return Vec2<T>{x-rhs.x, y-rhs.y};
     }
 
     constexpr auto operator-() const
     {
-        return Vec2<T>(-x1, -x2);
+        return Vec2<T>{-x, -y};
     }
 
     template<typename U>
-    requires std::integral<U> || std::floating_point<U>
+    requires numeric<U>
     constexpr auto operator*=(U rhs) const
     {
-        return Vec2<T>(x1*rhs, x2*rhs);
+        return Vec2<T>{x*rhs, y*rhs};
     } 
 
     constexpr auto magnitude() const
     {
-        return std::sqrt(x1^2 + x2^2);
+        return std::sqrt(x^2 + y^2);
     }
 };
 
 template<typename T>
-requires std::integral<T> || std::floating_point<T>
-constexpr auto dot(Vec2<T> lhs, Vec2<T> rhs)
+requires numeric<T>
+constexpr auto dot(const Vec2<T>& lhs, const Vec2<T>& rhs)
 {
-    return(lhs.x1*rhs.x1+lhs.x2*rhs.x2)
+    return (lhs.x*rhs.x+lhs.y*rhs.y);
 }
 
 using vec2i = Vec2<int>;
 using vec2u = Vec2<unsigned int>;
 using vec2f = Vec2<float>;
 
+
+template<typename T>
+requires numeric<T>
+struct Vec3
+{
+    T x = 0, y = 0, z = 0;
+
+    constexpr Vec3<T>() = default;
+    constexpr Vec3<T>(T x_, T y_, T z_) : x{x_}, y{y_}, z{z_} {}
+
+    constexpr auto operator+=(Vec3<T> rhs) const
+    {
+        return Vec3<T>{x+rhs.x, y+rhs.y, z+rhs.z};
+    }
+    constexpr auto operator-=(Vec3<T> rhs) const
+    {
+        return Vec3<T>{x-rhs.x, y-rhs.y, z-rhs.z};
+    }
+    constexpr auto operator-() const
+    {
+        return Vec3<T>{-x, -y, -z};
+    }
+    constexpr auto magnitude() const 
+    {
+        return std::sqrt(x^2 + y^2 + z^2);
+    }
+
+    template<typename U>
+    requires numeric<U>
+    constexpr auto operator*=(U rhs)
+    {
+        return Vec3<T>{x*rhs, y*rhs, z*rhs};
+    }
+};
+
+template<typename T>
+requires numeric<T>
+constexpr auto dot(const Vec3<T>& lhs, const Vec3<T>& rhs)
+{
+    return (lhs.x*rhs.x + lhs.y*rhs.y + lhs.z*rhs.z);
+}
+
+template<typename T>
+requires numeric<T>
+constexpr auto cross(const Vec3<T>& lhs, const Vec3<T>& rhs)
+{
+    return Vec3<T>{lhs.y*rhs.z - lhs.z*rhs.y,
+                   lhs.z*rhs.x - lhs.x*rhs.z,
+                   lhs.x*rhs.y - lhs.y*rhs.x};
+}
+
+using vec3i = Vec3<int>;
+using vec3f = Vec3<float>;
+using vec3u = Vec3<unsigned int>;
+
+}
